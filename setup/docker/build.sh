2e615e09c8b31d0cff11d005077381fe35708483
Matthew Jackson <matthew@hmjackson.com>
2023-10-09 10:00:56 +0100

    Use generic GID for Dockerfile

--- setup/docker/build.sh
+++ setup/docker/build.sh
@@ -3,7 +3,7 @@
 echo 'Building Dockerfile with image name jaxrl'
 docker build \
     --build-arg UID=$(id -u ${USER}) \
-    --build-arg GID=$(id -g ${USER}) \
+    --build-arg GID=1234 \
     --build-arg REQS="$(cat ../requirements-base.txt ../requirements-gpu.txt | tr '\n' ' ')" \
     -t jaxrl \
     .
